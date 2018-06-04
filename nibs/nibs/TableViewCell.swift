5317df18e07316e74827c2322e57463fbfdeecbe
AbrahamSM <38536521+AbrahamSM@users.noreply.github.com>
2018-06-04 11:32:43 -0500

    Add files via upload

---
+++ nibs/nibs/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  nibs
+//
+//  Created by macbookUser on 23/05/18.
+//  Copyright © 2018 macbookUser. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var actividad: UILabel!
+    @IBOutlet weak var subtitulo: UILabel!
+    @IBOutlet weak var imagen: UIImageView!
+    
+    
+}
